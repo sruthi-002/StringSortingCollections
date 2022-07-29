44c1e1535e3318f3c2bc1a011476c88fe268a0e0
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-07-29 14:51:27 +0530

    Add files via upload

---
+++ StringSortingCollections.java
@@ -0,0 +1,23 @@
+﻿package com.company;
+import java.util.Scanner;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Sort a = new Sort();
+        a.Sorting();
+    }
+}
+class Sort{
+    public static void Sorting() {
+        ArrayList<String> Sort = new ArrayList<String>();
+        System.out.println("Enter the Number of Elements");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i,temp;
+        for (i = 0; i < n; i++) {
+            Sort.add(in.next());
+        }
+        Collections.sort(Sort);
+        System.out.println(Sort);
+    }
+}
